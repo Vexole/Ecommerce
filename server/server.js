c6a86c0ef0150feb8c88d41a4176a036b2b4eb13
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-02-08 22:14:24 +0545

    First HTTP Request

--- server/server.js
+++ server/server.js
@@ -8,3 +8,12 @@ const app = express();
 app.use(bodyParser.json()); // readind data in json format
 app.use(bodyParser.urlencoded({extended: false})); // read all data type
 app.use(morgan('dev')); // log request in terminal
+
+
+app.get('/', (req, res, next) => {
+    res.send("First HTTP");
+});
+
+app.listen(3030, err => {
+    console.log("Hello");
+});
