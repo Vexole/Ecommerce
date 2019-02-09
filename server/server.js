27426123ef8e2e62d73a2635a4018da4d00934dd
Bhupesh Shrestha <bhupesh.shrestha@pdc01.lisnepal.com.np>
2019-02-09 09:38:52 +0545

    Preventing CORS error

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express'); // create routes
 const morgan = require('morgan'); // middleware for route
 const bodyParser = require('body-parser'); // data reader
 const mongoose = require('mongoose'); // mongodb agent
+const cors = require('cors'); // adding cors npm install cors --save
 
 const config = require('./config'); // db configuration
 
@@ -18,7 +19,7 @@ mongoose.connect(config.database, err => {
 app.use(bodyParser.json()); // readind data in json format
 app.use(bodyParser.urlencoded({extended: false})); // read all data type
 app.use(morgan('dev')); // log request in terminal
-
+app.use(cors()); 
 
 app.get('/', (req, res, next) => {
     res.send("First HTTP");
